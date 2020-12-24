93527b9d19f84fe6418d426008651e62f336cafc
dedayodeveloper <olaleye.adedayo@yahoo.com>
2020-12-24 10:32:59 +0100

    update
    Signed-off-by: dedayodeveloper <olaleye.adedayo@yahoo.com>

--- src/main/java/com/ptv/escort/Admin/GlobalExceptionHandler.java
+++ src/main/java/com/ptv/escort/Admin/GlobalExceptionHandler.java
@@ -1,30 +1,30 @@
-package com.ptv.escort.Admin;
-
-
-import java.io.IOException;
-import javax.servlet.http.HttpServletResponse;
-
-import com.ptv.escort.Response.ApiResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-
-
-    @ExceptionHandler(RuntimeException.class)
-    @ResponseBody
-    public ApiResponse<String> springHandleNotFound(HttpServletResponse response, Exception ex) throws IOException {
-        ApiResponse<String> error = new ApiResponse<>();
-        error.setResponse(ex.getMessage());
-        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
-        error.setMessage(ex.getMessage());
-        error.setError("Error Found");
-        error.setResponsecode("99");
-        return error;
-    }
-
-}
+//package com.ptv.escort.Admin;
+//
+//
+//import java.io.IOException;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import com.ptv.escort.Response.ApiResponse;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+//
+//
+//    @ExceptionHandler(RuntimeException.class)
+//    @ResponseBody
+//    public ApiResponse<String> springHandleNotFound(HttpServletResponse response, Exception ex) throws IOException {
+//        ApiResponse<String> error = new ApiResponse<>();
+//        error.setResponse(ex.getMessage());
+//        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+//        error.setMessage(ex.getMessage());
+//        error.setError("Error Found");
+//        error.setResponsecode("99");
+//        return error;
+//    }
+//
+//}
